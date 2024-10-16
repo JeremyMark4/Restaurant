2c388e5663d17af3da12fe1e5fcb263f51562227
JeremyMark4 <jeremymark751@gmail.com>
2024-10-16 13:05:01 +0300

    first commit

---
+++ app/Http/Controllers/CategoryNamesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category_names;
+use App\Http\Requests\StoreCategory_namesRequest;
+use App\Http\Requests\UpdateCategory_namesRequest;
+
+class CategoryNamesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategory_namesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category_names $category_names)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category_names $category_names)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategory_namesRequest $request, Category_names $category_names)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category_names $category_names)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/OrderDetailsTableController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order_details_table;
+use App\Http\Requests\StoreOrder_details_tableRequest;
+use App\Http\Requests\UpdateOrder_details_tableRequest;
+
+class OrderDetailsTableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOrder_details_tableRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Order_details_table $order_details_table)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Order_details_table $order_details_table)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateOrder_details_tableRequest $request, Order_details_table $order_details_table)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Order_details_table $order_details_table)
+    {
+        //
+    }
+}
